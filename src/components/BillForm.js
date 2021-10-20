276d1765f7c205ca9dba7164f51612003e231e9c
Mateusz Janowski <mat.krz.janowski@gmail.com>
2021-10-20 21:56:41 +0200

    Added styling

--- src/components/BillForm.js
+++ src/components/BillForm.js
@@ -1,13 +1,48 @@
 import { useState } from 'react';
-import {
-	toPercent,
-	calculateTotalPrice,
-	getTipOptions,
-	defaultPrice,
-	defaultTip,
-	tax,
-	currency
-} from '../utils/auxFunctions';
+import { toPercent, calculateTotalPrice, getTipOptions, defaultPrice, defaultTip, tax, currency } from '../utils/auxFunctions';
+
+const styles = {
+	formSection: {
+		display: 'flex',
+		flexDirection: 'column',
+		padding: 10,
+		gap: 5,
+		fontWeight: 500,
+		letterSpacing: 1,
+		fontSize: 18
+	},
+
+	container: {
+		display: 'flex',
+		flexDirection: 'column',
+		padding: 10,
+		gap: 5,
+		fontWeight: 500,
+		letterSpacing: 1,
+		fontSize: 18,
+		textAlign: 'center'
+	},
+
+	button: {
+		borderRadius: 6,
+		border: '2px solid #1982c4',
+		background: 'white',
+		letterSpacing: 1,
+		textTransform: 'uppercase'
+	},
+	formField: {
+		borderRadius: 6,
+		border: '1px solid #4f5d75',
+		background: 'white',
+		letterSpacing: 1,
+		paddingLeft: 5,
+		textAlign: 'left',
+		textTransform: 'uppercase'
+	},
+	totalPrice: {
+		color: '#38b000'
+	}
+};
 
 const BillForm = () => {
 	const [ formState, setFormState ] = useState({ price: defaultPrice, tip: defaultTip });
@@ -32,14 +67,14 @@ const BillForm = () => {
 
 	const billFormComponent = (
 		<form onSubmit={onFormSubmit} onChange={onFormChanged}>
-			<div>
-				<label htmlFor='price'>Price {currency}: </label>
-				<input id='price' defaultValue={formState.price} name='price' type='number' />
+			<div style={styles.formSection}>
+				<label htmlFor="price">Price {currency}: </label>
+				<input style={styles.formField} id="price" defaultValue={formState.price} name="price" type="number" />
 			</div>
 
-			<div>
-				<label htmlFor='price'>Tip %: </label>
-				<select id='tip' name='tip' defaultValue={defaultTip}>
+			<div style={styles.formSection}>
+				<label htmlFor="price">Tip %: </label>
+				<select style={styles.formField} id="tip" name="tip" defaultValue={defaultTip}>
 					{getTipOptions().sort().map((tipValue, id) => (
 						<option key={id} value={tipValue}>
 							{toPercent(tipValue)}
@@ -47,27 +82,32 @@ const BillForm = () => {
 					))}
 				</select>
 			</div>
-
-			<button type='submit'>Calculate</button>
+			<div style={styles.formSection}>
+				<button style={styles.button} type="submit">
+					Calculate
+				</button>
+			</div>
 		</form>
 	);
 
 	const totalPriceComponent = (
-		<div>
-			<h1>
+		<>
+			<h3>Total amount:</h3>
+			<h3 style={styles.totalPrice}>
 				{totalPrice} {currency}
-			</h1>
+			</h3>
 			<p>
 				<small>
-					Price includes <strong>{toPercent(tax)}%</strong> tax, and{' '}
-					<strong>{toPercent(formState.tip)}%</strong> tip
+					Price includes <strong>{toPercent(tax)}%</strong> tax, and <strong>{toPercent(formState.tip)}%</strong> tip
 				</small>
 			</p>
-			<button onClick={goBackHandle}>Back</button>
-		</div>
+			<button style={styles.button} onClick={goBackHandle}>
+				Back
+			</button>
+		</>
 	);
 
-	return <div>{priceCalculated ? totalPriceComponent : billFormComponent}</div>;
+	return <div style={styles.container}>{priceCalculated ? totalPriceComponent : billFormComponent}</div>;
 };
 
 export default BillForm;
